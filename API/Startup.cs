3ad5314c62b83cc1c772fd481c2c624a04eb363f
ismailermis@gmail.com <ismailermis@gmail.com>
2020-04-19 20:02:25 +0300

    8540p section 3 Cors config is done

--- API/Startup.cs
+++ API/Startup.cs
@@ -13,44 +13,44 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Persistence;
 
-namespace API
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace API {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<DataContext>(opt =>
-            {
-                opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+        public void ConfigureServices (IServiceCollection services) {
+            // services.AddCors (o => o.AddPolicy ("MyPolicy", builder => {
+            //     builder.AllowAnyOrigin ()
+            //         .AllowAnyMethod ()
+            //         .AllowAnyHeader ();
+            // }));
+            services.AddCors (options => options.AddPolicy ("ApiCorsPolicy", builder => {
+                builder.WithOrigins ("http://localhost:3000").AllowAnyMethod ().AllowAnyHeader ();
+            }));
+            services.AddDbContext<DataContext> (opt => {
+                opt.UseSqlite (Configuration.GetConnectionString ("DefaultConnection"));
             });
-            services.AddControllers();
+            services.AddControllers ();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
             }
 
             // app.UseHttpsRedirection();
 
-            app.UseRouting();
+            app.UseRouting ();
+            app.UseCors ("ApiCorsPolicy");
+            app.UseAuthorization ();
 
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
+            app.UseEndpoints (endpoints => {
+                endpoints.MapControllers ();
             });
         }
     }
--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,31 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 class App extends Component {
-  state = {
-    values: []
-  };
+	state = {
+		values: [],
+	};
 
-  componentDidMount() {
-     this.setState({
-       values : [{id:1,name:'values 101'},{id:2,name:'value 102'}]
-     })
-  }
+	componentDidMount() {
+		axios.get("http://localhost:5000/api/values/").then((response) => {
+			this.setState({
+				values: response.data,
+			});
+		});
+	}
 
-render(){
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-          <ul>
-            {this.state.values.map((value:any)=>(
-              <li>{value.id} --> {value.name}</li>
-            ))}
-          </ul>
-      </header>
-    </div>
-  );
-}
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<ul>
+						{this.state.values.map((value: any) => (
+							<li key={value.id}>
+								{value.id} --> {value.name}
+							</li>
+						))}
+					</ul>
+				</header>
+			</div>
+		);
+	}
 }
 export default App;
