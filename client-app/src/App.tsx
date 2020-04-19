41ca46162bcd66d108a1d9bb2179f67ff7cf2351
ismailermis@gmail.com <ismailermis@gmail.com>
2020-04-19 18:16:54 +0300

    show state from local values

---
+++ client-app/src/App.tsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  state = {
+    values: []
+  };
+
+  componentDidMount() {
+     this.setState({
+       values : [{id:1,name:'values 101'},{id:2,name:'value 102'}]
+     })
+  }
+
+render(){
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+          <ul>
+            {this.state.values.map((value:any)=>(
+              <li>{value.id} --> {value.name}</li>
+            ))}
+          </ul>
+      </header>
+    </div>
+  );
+}
+}
+export default App;
