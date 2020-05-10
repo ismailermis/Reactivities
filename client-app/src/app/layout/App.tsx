dcf4439e5b29fa74684b75e29e7088da980c4337
ismailermis@gmail.com <ismailermis@gmail.com>
2020-05-10 06:29:18 +0300

    show detail activity right side

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -5,13 +5,22 @@ import { IActivity } from "../models/activity";
 import "semantic-ui-css/semantic.min.css";
 import { NavBar } from "../../features/nav/NavBar";
 import { ActivityDashboard } from "../../features/activities/dashboard/ActivityDashboard";
-import { ActivityForm } from "../../features/activities/form/ActivityForm";
+import ActivityList from "../../features/activities/dashboard/ActivityList";
+
 interface IState {
 	activities: IActivity[];
 }
 
 const App = () => {
 	const [activities, setActivities] = useState<IActivity[]>([]);
+	const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(
+		null
+	);
+
+	const handleSelectedActivity = (id: string) => {
+		setSelectedActivity(activities.filter((a) => a.id == id)[0]);
+	};
+
 	useEffect(() => {
 		axios
 			.get<IActivity[]>("http://localhost:5000/api/activities/")
@@ -24,8 +33,11 @@ const App = () => {
 		<Fragment>
 			<NavBar />
 			<Container style={{ marginTop: "7em" }}>
-				<ActivityDashboard activities={activities} />
-				<ActivityForm />
+				<ActivityDashboard
+					activities={activities}
+					selectActivity={handleSelectedActivity}
+					selectedActivity={selectedActivity!}
+				/>
 			</Container>
 		</Fragment>
 	);
